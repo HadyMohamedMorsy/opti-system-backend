ae39cc4d8bf76205fb0126013178d3d9ffc28e18
hadySwuperCommerce <Hady.Mohamed@supercommerce.io>
2025-12-26 09:51:41 +0200

    back

--- src/migrations/1766735138477-secoundMigration.ts
+++ src/migrations/1766735138477-secoundMigration.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class SecoundMigration1764581776360 implements MigrationInterface {
-    name = 'SecoundMigration1764581776360'
+export class SecoundMigration1766735138477 implements MigrationInterface {
+    name = 'SecoundMigration1766735138477'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "general_settings" DROP COLUMN "name"`);
@@ -10,6 +10,18 @@ export class SecoundMigration1764581776360 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "general_settings" DROP COLUMN "meta_title"`);
         await queryRunner.query(`ALTER TABLE "general_settings" DROP COLUMN "meta_description"`);
         await queryRunner.query(`ALTER TABLE "general_settings" DROP COLUMN "meta_keywords"`);
+        await queryRunner.query(`ALTER TABLE "blogs" DROP COLUMN "title"`);
+        await queryRunner.query(`ALTER TABLE "blogs" DROP COLUMN "sub_title"`);
+        await queryRunner.query(`ALTER TABLE "blogs" DROP COLUMN "description"`);
+        await queryRunner.query(`ALTER TABLE "blogs" DROP COLUMN "short_description"`);
+        await queryRunner.query(`ALTER TABLE "blogs" DROP COLUMN "meta_title"`);
+        await queryRunner.query(`ALTER TABLE "blogs" DROP COLUMN "meta_description"`);
+        await queryRunner.query(`ALTER TABLE "sub_category" DROP COLUMN "description"`);
+        await queryRunner.query(`ALTER TABLE "sub_category" DROP COLUMN "name"`);
+        await queryRunner.query(`ALTER TABLE "sub_category" DROP COLUMN "icon"`);
+        await queryRunner.query(`ALTER TABLE "categories" DROP COLUMN "name"`);
+        await queryRunner.query(`ALTER TABLE "categories" DROP COLUMN "icon"`);
+        await queryRunner.query(`ALTER TABLE "categories" DROP COLUMN "description"`);
         await queryRunner.query(`ALTER TABLE "general_settings" ADD "content" json NOT NULL`);
         await queryRunner.query(`ALTER TABLE "general_settings" ADD "store_email" character varying`);
         await queryRunner.query(`ALTER TABLE "general_settings" ADD "store_phone" character varying`);
@@ -20,9 +32,15 @@ export class SecoundMigration1764581776360 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "general_settings" ADD "smtp_email" character varying`);
         await queryRunner.query(`ALTER TABLE "general_settings" ADD "smtp_password" character varying`);
         await queryRunner.query(`ALTER TABLE "general_settings" ADD "maintenance_mode" boolean DEFAULT false`);
+        await queryRunner.query(`ALTER TABLE "blogs" ADD "content" json`);
+        await queryRunner.query(`ALTER TABLE "sub_category" ADD "content" json NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "categories" ADD "content" json`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "categories" DROP COLUMN "content"`);
+        await queryRunner.query(`ALTER TABLE "sub_category" DROP COLUMN "content"`);
+        await queryRunner.query(`ALTER TABLE "blogs" DROP COLUMN "content"`);
         await queryRunner.query(`ALTER TABLE "general_settings" DROP COLUMN "maintenance_mode"`);
         await queryRunner.query(`ALTER TABLE "general_settings" DROP COLUMN "smtp_password"`);
         await queryRunner.query(`ALTER TABLE "general_settings" DROP COLUMN "smtp_email"`);
@@ -33,6 +51,18 @@ export class SecoundMigration1764581776360 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "general_settings" DROP COLUMN "store_phone"`);
         await queryRunner.query(`ALTER TABLE "general_settings" DROP COLUMN "store_email"`);
         await queryRunner.query(`ALTER TABLE "general_settings" DROP COLUMN "content"`);
+        await queryRunner.query(`ALTER TABLE "categories" ADD "description" character varying`);
+        await queryRunner.query(`ALTER TABLE "categories" ADD "icon" character varying`);
+        await queryRunner.query(`ALTER TABLE "categories" ADD "name" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "sub_category" ADD "icon" character varying`);
+        await queryRunner.query(`ALTER TABLE "sub_category" ADD "name" character varying(255) NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "sub_category" ADD "description" text`);
+        await queryRunner.query(`ALTER TABLE "blogs" ADD "meta_description" text`);
+        await queryRunner.query(`ALTER TABLE "blogs" ADD "meta_title" character varying`);
+        await queryRunner.query(`ALTER TABLE "blogs" ADD "short_description" text`);
+        await queryRunner.query(`ALTER TABLE "blogs" ADD "description" text`);
+        await queryRunner.query(`ALTER TABLE "blogs" ADD "sub_title" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "blogs" ADD "title" character varying NOT NULL`);
         await queryRunner.query(`ALTER TABLE "general_settings" ADD "meta_keywords" text`);
         await queryRunner.query(`ALTER TABLE "general_settings" ADD "meta_description" text`);
         await queryRunner.query(`ALTER TABLE "general_settings" ADD "meta_title" character varying`);
